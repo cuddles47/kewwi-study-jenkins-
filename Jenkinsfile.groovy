1fdc5f6a11db16f7e4149254183ba25b0134da63
_kewwi ng_ <102494428+cuddles47@users.noreply.github.com>
2024-08-07 09:55:47 +0700

    Create Jenkinsfile.groovy

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,10 @@
+pipeline {
+    agent any
+    stages {
+        stage('Clone'){
+            step {
+                git 'https://github.com/cuddles47/kewwi-study-jenkins-'
+            }
+        }
+    }
+}
